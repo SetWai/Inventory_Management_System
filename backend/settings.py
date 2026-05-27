d71a863aef94f671cde10308a574862cae3e572e
SetWai <sw.setwai@gmail.com>
2026-05-27 17:25:02 +0400

    add static_root in setting.py

--- backend/settings.py
+++ backend/settings.py
@@ -124,6 +124,8 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
